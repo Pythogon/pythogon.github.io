e9a86fde0651585ac284e89580cb0d591b2f2d4d
iiSkyii <iiskyii@github.com>
2019-02-01 18:19:50 +0000

    Neatened up and switched on * web\titlerandomiser.js

--- web/titlerandomiser.js
+++ web/titlerandomiser.js
@@ -1,6 +1,14 @@
 function titleRandomiser() {
-    var sub = Math.floor(Math.random() * Math.floor(20));
-    if(sub == 1) {
-        document.getElementById('titlechanger').innerHTML = 'Sub to PewDiePie';
-    }
+  switch (Math.floor(Math.random() * Math.floor(100))) {
+    default:
+      response = 'Pythogon';
+      break;
+    case 1:
+      response = 'Sub to Pewds'
+      break;
+    case 18:
+      response = 'Pytec 2: Electric Boogaloo'
+      break;
+  }
+  document.getElementById('titlechanger').innerHTML = response;
 }
