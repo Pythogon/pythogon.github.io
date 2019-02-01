fb28c92dc88039d1c4bcebc97f8cb351392de781
th0masc00l <thomasfmanship@googlemail.com>
2019-02-01 22:11:23 +0000

    Testing something and moved logo thingy for one thing * docs\css\styles.css * docs\index.html * docs\titrand.js

--- docs/titrand.js
+++ docs/titrand.js
@@ -5,6 +5,8 @@ function titleRandomiser() {
       break;
     case 1:
       txt = 'Sub to Pewds'
+      document.getElementById('logo').style.right = "25vw";
+      document.getElementById('logo').style.marginBottom = "-12vw";
       break;
     case 18:
       txt = 'Pytec 2: Electric Boogaloo'
