39c87ba577cea643672375aaef29db6339af61b3
iiSkyii <iiskyii@github.com>
2019-02-01 17:50:23 +0000

    Declutter * web\index.html + web\titlerandomiser.js

---
+++ web/titlerandomiser.js
@@ -0,0 +1,6 @@
+function titleRandomiser() {
+    var sub = Math.floor(Math.random() * Math.floor(20));
+    if(sub == 1) {
+        document.getElementById('titlechanger').innerHTML = 'Sub to PewDiePie';
+    }
+}
