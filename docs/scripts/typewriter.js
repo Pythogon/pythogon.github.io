b44eee7c96d48b17680f2f707e59074728a5cda7
iiSkyii <iiskyii@github.com>
2019-02-02 11:28:38 +0000

    Finally! WOO * docs\index.html + docs\scripts\loader.js + docs\scripts\typewriter.js

---
+++ docs/scripts/typewriter.js
@@ -0,0 +1,11 @@
+var i = 0;
+var text = 'PyTec Hub'
+var speed = 100
+
+function typewriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typewriter, speed);
+  }
+}
