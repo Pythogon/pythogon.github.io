d07242ec0647f365625ae4c9da1795bff0793be3
iiSkyii <iiskyii@github.com>
2019-02-02 20:19:55 +0000

    Fixing #1 and the juciest timings * docs\scripts\typewriter.js

--- docs/scripts/typewriter.js
+++ docs/scripts/typewriter.js
@@ -1,9 +1,9 @@
 var i = 0;
-var text = 'Pythogon    Hub'
+var text = 'Pythogon      Hub'
 var speed = 100
 
 function typewriter() {
-  if (i < txt.length) {
+  if (i < text.length) {
     document.getElementById("type").innerHTML += text.charAt(i);
     i++;
     setTimeout(typewriter, speed);
