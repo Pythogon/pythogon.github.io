e30f130608733533acbd91ed0f4014a0ce7b8354
th0masc00l <thomasfmanship@googlemail.com>
2019-02-07 20:14:31 +0000

    trying to get closing working * dox\css\styles.css * dox\index.html * dox\scripts\sidebar.js

--- dox/scripts/sidebar.js
+++ dox/scripts/sidebar.js
@@ -1,4 +1,10 @@
 function sidebar() {
   document.getElementById("sidebar").style.width = "12vw";
   document.getElementById("sidebar").style.paddingLeft = "0.7vw";
+  document.getElementById("sidebar-close").style.visibility = "shown";
+}
+function sidebarClose() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("sidebar").style.paddingLeft = "0";
+  document.getElementById("sidebar-close").style.visibility = "hidden";
 }
